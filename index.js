6862b0cd797478896014762d3e55a317a8bac979
JonathanMary <jon85ma@gmail.com>
2020-12-14 22:09:17 +0100

    MVP done

--- index.js
+++ index.js
@@ -28,11 +28,16 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+    -counter1 is a const calling a function, counter2 is a function.
   
   2. Which of the two uses a closure? How can you tell?
+    -counter1 uses a closure, there is a nested function into counterMaker();
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+    counter2 be better?  
+    -counter1 would be preferable if you want to use it for multiple variables.
+    -counter2 would be preferable if you want to use the count variable.
+
 */
 
 // counter1 code
@@ -63,7 +68,7 @@ NOTE: This will be a callback function for the tasks below
 */
 
 function inning(/*Code Here*/){
-    /*Code Here*/
+    return Math.floor(Math.random()*3);
 }
 
 
@@ -81,18 +86,34 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, num){
+  const result = {
+    "Home": 0,
+    "Away": 0
+  };
+  for(let i=0; i< num; i++){
+    result["Home"] += inning();
+    result["Away"] += inning();
+  }
+  return result;
 }
+
+//console.log(finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+  const score = {
+    Home: inning(),
+    Away: inning()
+  }
+  return score;
 }
+
+console.log(getInningScore(inning));
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
@@ -136,7 +157,7 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
+function scoreboard(getInningScore, inning, numInnings) {
   /* CODE HERE */
 }
 
